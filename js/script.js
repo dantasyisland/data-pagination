871b38b9d22c4bb6e94324b95404cce58f229814
dantasyisland <dantothelastdrop@yahoo.com>
2021-11-15 23:50:56 -0600

    started to draft

--- js/script.js
+++ js/script.js
@@ -11,13 +11,46 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+
+
+//        EXAMPLE - Student list item:
+
+// <li class="student-item cf">
+//    <div class="student-details">
+//       <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+//       <h3>Ethel Dean</h3>
+//       <span class="email">ethel.dean@example.com</span>
+//    </div>
+//    <div class="joined-details">
+//       <span class="date">Joined 12-15-2005</span>
+//    </div>
+// </li>
+
+// li has two children which are divs - student-details has 3 children - pic name email
+
+console.log(data);
+let ul = document.querySelector(".student-list")
+let li = document.createElement('li');
+let span = document.createElement('span');
+span.innerText = 'THE SPAN';
+// li.appendChild(span);
+// ul.appendChild(li);
+
+let studentDetails = document.createElement('div');
+studentDetails.className = 'student-details';
+studentDetails.innerText = 'teststs'
+
+
+let profilePic = document.createElement('img');
+profilePic.src = data[0].picture.large;
+li.appendChild(profilePic)
+// li.appendChild.studentDetails;
+ul.appendChild();
 
 
 /*
