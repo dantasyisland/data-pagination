b58770b792abe350d61a19c0260b79de7f76f3df
dantasyisland <dantothelastdrop@yahoo.com>
2021-11-20 22:54:46 -0600

    Build build build baby

--- js/templateScript.js
+++ js/templateScript.js
@@ -12,6 +12,7 @@ For assistance:
 */
 
 
+let itemsPerPage = 9;
 
 /*
 Create the `showPage` function
@@ -23,12 +24,41 @@ This function will create and insert/append the elements needed to display a "pa
  * @page {number} page
  */
 function showPage(list,page) {
-   const startIndex = (page * 9) - 9;
-   const endIndex = page * 9;
-   const ul = document.querySelector('.student-list');
-   ul.innerHTML = '';
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
    for(let i = 0; i < list.length; i++) {
       if(i>=startIndex && i < endIndex) {
+         function createElement(elementName, className, innertext) {
+            const element = document.createElement(elementName);
+            if(className) element.className = className;
+            if(innertext) element.innerText = innertext;
+            return element;
+         }
+
+      const studentHTML = `<li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="${data[i].picture.large}">
+           <h3>${data[i].name.first} ${data[i].name.last}</h3>
+           <span class="email">${data[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${data[i].registered.date}</span>
+         </div>
+       </li>`
+
+         studentList.insertAdjacentHTML("beforeend",studentHTML); // before end because 1-9 not 9-1
+
+
+
+
+
+
+
+
+      }
+
 
       //  <li class="student-item cf">
       //    <div class="student-details">
@@ -43,14 +73,12 @@ function showPage(list,page) {
 
 
 
-         console.log(list[i]);
       }
 
-   }
-   console.log(ul);
+
 }
 
-showPage(data, 1);
+
 
 
 
@@ -59,6 +87,42 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+    // create a variable to calculate the number of pages needed
+   let numOfPages = Math.ceil(list.length/itemsPerPage);
+  // select the element with a class of `link-list` and assign it to a variable
+   const linkList = document.querySelector('.link-list');
 
+  // set the innerHTML property of the variable you just created to an empty string
+   linkList.innerHTML = '';
+  // loop over the number of pages needed
+     for(let i = 1; i <= numOfPages; i++) {
+        const button = `<li><button type="button">${i}</button></li>`
+        linkList.insertAdjacentHTML('beforeend',button)
+
+        console.log(i);
+     }
+    // create the elements needed to display the pagination button
+    // insert the above elements
+
+  // give the first pagination button a class of "active"
+     document.querySelector('button').className = "active";
+
+  // create an event listener on the `link-list` element
+    // if the click target is a button:
+      // remove the "active" class from the previous button
+      // add the active class to the clicked button
+      // call the showPage function passing the `list` parameter and page to display as arguments
+
+      console.log(list);
+
+   linkList.addEventListener('click', e => {
+      if (e.target.tagName === 'BUTTON') {
+      console.log("CLICKFOOL!");
+      }
+   })
+}
+addPagination(data);
+showPage(data, 4);
 
 // Call functions
