ee6a3aa7374adef7e8ee5cf2788149a5faf6fe71
dantasyisland <dantothelastdrop@yahoo.com>
2021-11-22 17:26:34 -0600

    Finished initial build - working on search

--- js/script.js
+++ js/script.js
@@ -11,86 +11,75 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+
+let itemsPerPage = 9;
+
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const studentList = document.querySelector('.student-list');
 
+/**
+ * @param {Object[]} list
+ * @page {number} page
+ */
+function showPage(list,page) {
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   studentList.innerHTML = '';
+   for(let i = 0; i < list.length; i++) {
+      if(i>=startIndex && i < endIndex) {
+         function createElement(elementName, className, innertext) {
+            const element = document.createElement(elementName);
+            if(className) element.className = className;
+            if(innertext) element.innerText = innertext;
+            return element;
+         }
 
+      const studentHTML = `<li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="${data[i].picture.large}">
+           <h3>${data[i].name.first} ${data[i].name.last}</h3>
+           <span class="email">${data[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${data[i].registered.date}</span>
+         </div>
+       </li>`
 
-//        EXAMPLE - Student list item:
-
-// Function will create 9 li from two functions that create divs
-// pagination through the array
-
-// li has two children which are divs - student-details has 3 children - pic name email
-
-
-
-
-
-
-
-let i = 0;
-
-const ul = document.querySelector('.student-list')
-
-// ------------------
-const li = document.createElement('li');
-li.className = 'student-item cf';
-// ----------------
-const studentDetailsDiv = document.createElement('div');
-studentDetailsDiv.className = 'student-details';
-
-// ------ 3 children - Parent Student Details Div
-
-const profilePic = document.createElement('img');
-profilePic.className = 'avatar';
-profilePic.src = data[i].picture.large;
-profilePic.alt = 'Profile Picture';
-
-const h3 = document.createElement('h3');
-h3.innerText = `${data[i].name.first} ${data[i].name.last}`
-
-const email = document.createElement('span');
-email.className = 'email';
-email.innerText = `${data[i].email}`;
-
-// ----- Append
-studentDetailsDiv.appendChild(profilePic);
-studentDetailsDiv.appendChild(h3);
-studentDetailsDiv.appendChild(email);
-
-
-// ---------------
-
-const joinedDetailsDiv = document.createElement('div');
-joinedDetailsDiv.className = 'joined-details';
-
-const dateJoined = document.createElement('span');
-dateJoined.className = 'joined-details';
-dateJoined.innerText = `Joined ${data[i].registered.date}`
-
-
-// ------ Append
-joinedDetailsDiv.appendChild(dateJoined)
-
-
-li.appendChild(studentDetailsDiv);
-li.appendChild(joinedDetailsDiv);
-
-// li two children the divs
-// StudentDetails - 3 children img h3 span email
-// Joined Details - 1 child - span date
-
-// li.appendChild(span);
-// ul.appendChild(li);
+         studentList.insertAdjacentHTML("beforeend",studentHTML); // before end because 1-9 not 9-1
 
 
 
 
-// li.appendChild.studentDetails;
-ul.appendChild(li);
+
+
+
+
+      }
+
+
+      //  <li class="student-item cf">
+      //    <div class="student-details">
+      //      <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+      //      <h3>Ethel Dean</h3>
+      //      <span class="email">ethel.dean@example.com</span>
+      //    </div>
+      //    <div class="joined-details">
+      //      <span class="date">Joined 12-15-2005</span>
+      //    </div>
+      //  </li>
+
+
+
+      }
+
+
+}
+
+
+
 
 
 /*
@@ -98,6 +87,80 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+    // create a variable to calculate the number of pages needed
+   let numOfPages = Math.ceil(list.length/itemsPerPage);
+  // select the element with a class of `link-list` and assign it to a variable
+   const linkList = document.querySelector('.link-list');
 
+  // set the innerHTML property of the variable you just created to an empty string
+   linkList.innerHTML = '';
+  // loop over the number of pages needed
+     for(let i = 1; i <= numOfPages; i++) {
+        const button = `<li><button type="button">${i}</button></li>`
+        linkList.insertAdjacentHTML('beforeend',button)
+
+        console.log(i);
+     }
+    // create the elements needed to display the pagination button
+    // insert the above elements
+
+  // give the first pagination button a class of "active"
+     document.querySelector('button').className = "active";
+
+  // create an event listener on the `link-list` element
+    // if the click target is a button:
+      // remove the "active" class from the previous button
+      // add the active class to the clicked button
+      // call the showPage function passing the `list` parameter and page to display as arguments
+
+      console.log(list);
+
+   linkList.addEventListener('click', e => {
+      if (e.target.tagName === 'BUTTON') {
+         document.querySelector('.active').className = '';
+         e.target.className = "active";
+         showPage(data,e.target.innerText)
+      }
+   })
+}
+addPagination(data);
+// showPage(data, 1);
+
+
+
+
+
+
+const h2 = document.querySelector('h2');
+const search = `<label for="search" class="student-search">
+                     <span>Search by name</span>
+                     <input id="search" placeholder="Search by name...">
+                     <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+                </label>`
+h2.insertAdjacentHTML("afterend",search);
+let searchInput = document.querySelector('#search');
+let searchValue = searchInput.textValue; /// redunant
+
+searchInput.addEventListener('keyup', e => {
+   console.log(e.target.value);
+   removeChildren();
+   for(i = 0; i<=data.length; i++) {
+
+
+   }
+});
+
+if (!searchValue) {
+   showPage(data,1);
+}
+
+
+function removeChildren()  {
+ while(studentList.firstChild) {
+    studentList.removeChild(studentList.firstChild)
+ }
+}
+// removeChildren();
 
 // Call functions
