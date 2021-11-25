ab6a26afb62a2815f4a0857090db66e69a3934de
dantasyisland <dantothelastdrop@yahoo.com>
2021-11-24 20:58:51 -0600

    Fixed search feature to include no results

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*
+/*s
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
@@ -6,58 +6,28 @@ FSJS Project 2 - Data Pagination and Filtering
 const itemsPerPage = 9;
 const studentList = document.querySelector('.student-list');
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
 
-/**
- * @param {Object[]} list
- * @page {number} page
- */
 function showPage(list, page) {
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = page * itemsPerPage;
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-
-         const studentHTML = `<li class="student-item cf">
-         <div class="student-details">
-           <img class="avatar" src="${data[i].picture.large}">
-           <h3>${data[i].name.first} ${data[i].name.last}</h3>
-           <span class="email">${data[i].email}</span>
-         </div>
-         <div class="joined-details">
-           <span class="date">Joined ${data[i].registered.date}</span>
-         </div>
-       </li>`
-
-         studentList.insertAdjacentHTML("beforeend", studentHTML); // before end because 1-9 not 9-
-
+         const studentHTML = `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src="${list[i].picture.large}">
+                  <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                  <span class="email">${list[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">Joined ${list[i].registered.date}</span>
+               </div>
+            </li>`;
+         studentList.insertAdjacentHTML("beforeend", studentHTML);
       }
-
-
-      //  <li class="student-item cf">
-      //    <div class="student-details">
-      //      <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-      //      <h3>Ethel Dean</h3>
-      //      <span class="email">ethel.dean@example.com</span>
-      //    </div>
-      //    <div class="joined-details">
-      //      <span class="date">Joined 12-15-2005</span>
-      //    </div>
-      //  </li>
-
-
-
    }
-
-
 }
-
-
-
 
 
 /*
@@ -66,33 +36,18 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 function addPagination(list) {
-   // create a variable to calculate the number of pages needed
+
    let numOfPages = Math.ceil(list.length / itemsPerPage);
-   // select the element with a class of `link-list` and assign it to a variable
    const linkList = document.querySelector('.link-list');
 
-   // set the innerHTML property of the variable you just created to an empty string
    linkList.innerHTML = '';
-   // loop over the number of pages needed
+
    for (let i = 1; i <= numOfPages; i++) {
       const button = `<li><button type="button">${i}</button></li>`
       linkList.insertAdjacentHTML('beforeend', button)
-
-      console.log(i);
    }
-   // create the elements needed to display the pagination button
-   // insert the above elements
 
-   // give the first pagination button a class of "active"
    document.querySelector('button').className = "active";
-
-   // create an event listener on the `link-list` element
-   // if the click target is a button:
-   // remove the "active" class from the previous button
-   // add the active class to the clicked button
-   // call the showPage function passing the `list` parameter and page to display as arguments
-
-   console.log(list);
 
    linkList.addEventListener('click', e => {
       if (e.target.tagName === 'BUTTON') {
@@ -102,13 +57,63 @@ function addPagination(list) {
       }
    })
 }
-addPagination(data);
+
+
 showPage(data, 1);
+addPagination(data);
+
+const h2 = document.querySelector('h2');
+const search = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>`
 
 
+h2.insertAdjacentHTML("afterend", search);
 
+let searchInput = document.querySelector('.student-search');
+let searchValue = document.querySelector('input');
 
+function searchTable(input, list) {
+   studentList.innerHTML = '';
+   let newList = []
 
+   for (let i = 0; i <= list.length - 1; i++) {
+      if (input.length != 0 && (list[i].name.first.toLowerCase().includes(input.toLowerCase()) || list[i].name.last.toLowerCase().includes(input.toLowerCase()))) {
+         newList.push(list[i]);
+         showPage(newList, 1);
+         addPagination(newList);
 
+      }
+      //  else if (input.length != 0 && (list[i].name.first.toLowerCase().includes(input.toLowerCase()) || list[i].name.last.toLowerCase().includes(input.toLowerCase()))) {
+      //
+      //
+      // }
+   }
+   if (newList.length == 0) {
+      studentList.innerHTML = `<span>Sorry! No results found. Please search again</span>`;
+      document.querySelector('.link-list').innerHTML = '';
+   }
+   console.log(newList);
+}
 
-// Call functions
+// Both call search function
+searchInput.addEventListener('keyup', e => {
+   e.preventDefault();
+   if (searchValue.value == 0) {
+      showPage(data, 1)
+      addPagination(data);
+
+   } else if (searchValue.value != 0) {
+      searchTable(searchValue.value, data)
+   }
+});
+
+searchInput.addEventListener('click', e => {
+   e.preventDefault();
+   if (e.target.tagName === 'BUTTON' || e.target.tagName === 'IMG') {
+      searchValue.value = '';
+   }
+})
