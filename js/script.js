6b1ad413db0df1f26699c4bc5bb1c1cb3ad9db3f
dantasyisland <dantothelastdrop@yahoo.com>
2021-11-24 22:01:59 -0600

    Created ReadME - deleted old code - started comments and cleanup

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
+This project will take an array of student objects and display the student's information along with their picture. Buttons will be added for pagination.
+This array is also searchable and dynamically updates as the user types or submits search information.
 */
+
+
+
 
 const itemsPerPage = 9;
 const studentList = document.querySelector('.student-list');
 const linkList = document.querySelector('.link-list');
 
+
+
+/**
+ * This function loops through the array of students and will generate the HTML needed to display nine list elements per page
+ * @param {array} list Array of student objects
+ * @param {number} page The number of students per page
+ */
+
 function showPage(list, page) {
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = page * itemsPerPage;
+
    studentList.innerHTML = '';
+
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
          const studentHTML = `
@@ -29,10 +44,11 @@ function showPage(list, page) {
    }
 }
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+/**
+ * This function creates and inserts the elements needed for pagination buttons.
+ * @param {array} list Array of student objects
+ *
+ */
 
 function addPagination(list) {
    const numOfPages = Math.ceil(list.length / itemsPerPage);
@@ -50,13 +66,10 @@ function addPagination(list) {
       if (e.target.tagName === 'BUTTON') {
          document.querySelector('.active').className = '';
          e.target.className = 'active';
-         showPage(data, e.target.innerText);
+         showPage(data, e.target.textContent);
       }
    });
 }
-
-showPage(data, 1);
-addPagination(data);
 
 const h2 = document.querySelector('h2');
 const search = `
@@ -103,6 +116,10 @@ searchInput.addEventListener('keyup', (e) => {
 searchInput.addEventListener('click', (e) => {
    e.preventDefault();
    if (e.target.tagName === 'BUTTON' || e.target.tagName === 'IMG') {
+      searchTable(searchValue.value, data);
       searchValue.value = '';
    }
 });
+
+showPage(data, 1);
+addPagination(data);
